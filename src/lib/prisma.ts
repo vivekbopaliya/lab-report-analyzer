bda6d4c81ef8e68d4d2ab10b73044a8ee124bb79
vivekbopaliya <vivekpatel1nov@gmail.com>
2025-07-01 11:17:26 +0530

    setting up

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import "server-only";
+
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var cachedPrisma: PrismaClient;
+}
+
+let db: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  db = global.cachedPrisma;
+}
+
+export const prisma = db;
